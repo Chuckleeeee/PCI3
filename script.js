e4ac9ebc3d29fee819330881a5ae44c4a897780c
Chuckle <chucklecharlieh@gmail.com>
2026-08-26 18:45:12 +0100

    Civilisations

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Automatically update the copyright year
-document.getElementById("year").textContent = new Date().getFullYear();
-
-
 // Small reveal animation when the page loads
 window.addEventListener("load", () => {
     document.body.classList.add("loaded");
